2fa3747ae105a534dc7065c72b685e9f9fbcec68
Rachel Yordán <rachelyordan@gmail.com>
2024-07-03 22:49:23 +0100

    fix: update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // production url of the docs
-  url: 'https://BeLaws14.github.io',
+  url: 'https://kahboom.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/brodycademy/',
